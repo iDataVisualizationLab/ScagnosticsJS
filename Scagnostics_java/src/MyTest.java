fa36bd54ea669c164bbd13f49e4f76c2790bd70e
Pham Van Vung <phamvanvung@gmail.com>
2018-08-03 18:16:15 -0500

    Completed testings of - hexagon binning - convex hull - concave hull - spanning tree

---
+++ Scagnostics_java/src/MyTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author idvlab
+ */
+public class MyTest {
+    public static void main(String[] args){
+        double x = 1.9;
+        int y = (int)x;
+        System.out.println("y: " + y);
+    }
+}
